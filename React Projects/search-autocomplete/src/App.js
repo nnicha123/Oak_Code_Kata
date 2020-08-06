4347c2a846308b0f62b44f1849878635e88cb133
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-08-06 14:58:46 +0700

    following instructions

---
+++ React Projects/search-autocomplete/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AutoCompleted from './AutoCompleted'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AutoCompleted />
+      </div>
+    );
+  }
+}
+
+export default App;
